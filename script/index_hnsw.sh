28d0bd8f07cd6883a5c06a2cfd3ccbc17394b086
qfshen <qfshen5@yahoo.com>
2025-04-16 21:26:57 +0800

    run hnsw

--- script/index_hnsw.sh
+++ script/index_hnsw.sh
@@ -1,22 +1,43 @@
-
 cd ..
 
-g++ -o ./src/index_hnsw ./src/index_hnsw.cpp -I ./src/ -O3
+g++ -o ./src/index_hnsw ./src/index_hnsw.cpp -I ./src/ -O3 -I /usr/include/eigen3 
 
 efConstruction=500
-M=16
-data='gist'
+M=32
+datasets=('sift' 'deep1M')
 
+for data in "${datasets[@]}"
+do  
+    for adaptive in {0..2}
+    do
 
-echo "Indexing - ${data}"
+        if [ $adaptive -ne 0 ];then
+            echo "Skipping adaptive=${adaptive} for dataset ${data}"
+            continue
+        fi  
 
-data_path=./data/${data}
-index_path=./data/${data}
+        echo "Indexing - ${data}"
 
-data_file="${data_path}/${data}_base.fvecs"
-index_file="${index_path}/${data}_ef${efConstruction}_M${M}.index"
-./src/index_hnsw -d $data_file -i $index_file -e $efConstruction -m $M
+        data_path=/data/vector_datasets/${data}
+        index_path=/data/tmp/hnsw/${data}
 
-data_file="${data_path}/O${data}_base.fvecs"
-index_file="${index_path}/O${data}_ef${efConstruction}_M${M}.index"
-./src/index_hnsw -d $data_file -i $index_file -e $efConstruction -m $M
+        if [ ! -d "$index_path" ]; then 
+            mkdir -p "$index_path"
+        fi
+
+        if [ $adaptive == "0" ] # raw vectors 
+        then
+            data_file="${data_path}/${data}_base.fvecs"
+            centroid_file="${data_path}/${data}_centroid_${C}.fvecs"
+        else                    # preprocessed vectors                  
+            data_file="${data_path}/O${data}_base.fvecs"
+            centroid_file="${data_path}/O${data}_centroid_${C}.fvecs"
+        fi
+
+        # 0 - IVF, 1 - IVF++, 2 - IVF+
+        index_file="${index_path}/O${data}_ef${efConstruction}_M${M}.index"
+        echo $index_file
+
+        ./src/index_hnsw -d $data_file -i $index_file -e $efConstruction -m $M
+    done
+done
